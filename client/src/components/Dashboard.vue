393631cce84a180bc0ce93a46c3dd6819fabfc76
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-18 16:46:30 +0200

    Composant Dashboard

---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,41 @@
+<template>
+    <table>
+        <tbody>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Count</th>
+        </tr>
+        <template v-for="movie in movies">
+            <tr v-bind:key="movie.id">
+                <td>{{ movie.id }}</td>
+                <td>{{ movie.title }}</td>
+                <td>{{ movie.count }}</td>
+            </tr>
+        </template>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "Dashboard",
+
+        data() {
+            return {
+                movies: []
+            }
+        },
+
+        async created() {
+            await this.getMovies()
+        },
+
+        methods: {
+            async getMovies() {
+                const response = await axios.get('http://localhost:8000/movies')
+                this.movies = response.data
+            }
+        }
+    }
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import Dashboard from './components/Dashboard.vue'
 
 Vue.config.productionTip = false
 
+Vue.user(VueRouter)
+
+const routes = [
+    {path: '/', component: Dashboard}
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+    router,
+    render: h => h(Dashboard)
+}).$mount('#app')
