d9fb0123a401c5cfa6abbdb53f7923eb8a6da718
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-18 17:15:37 +0200

    HTML Dashboard

--- client/src/main.js
+++ client/src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(Auth, {
     issuer: 'https://dev-846151.oktapreview.com /oauth2/default',
-    client_id: '{YOUR_CLIENT_ID}',
+    client_id: '0oagauc9sdZmiDH0r0h7',
     redirect_uri: 'http://localhost:8080/implicit/callback',
     scope: 'openid profile email'
 })
